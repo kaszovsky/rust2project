4a7d58833bb1bde44eb0690b9226b2f1e9f4a88c
kaszovsky <kasza@interia.pl>
2023-12-28 13:18:56 +0100

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,7 @@ fn main() {
         println!("--vlag wys wys vlag"); // "--vlag shows flag" in afrikaans
     } else if args[1] == "--vlag"{
         let hex1 = "000000446974206973206e696520276e20766c6167206e6965";
-        let hex2 = "00000000000000000000000000000000000000000000000000";
-        
-        let bytes1 = decode(hex1).expect("ongeldige heks1");
-        let bytes2 = decode(hex2).expect("ongeldige heks2");
-      
-        let magic: Vec<u8> = bytes1
-            .iter()
-            .zip(bytes2.iter())
-            .map(|(&b1, &b2)| b1 ^ b2)
-            .collect();
-       
-        let text = String::from_utf8(magic).expect("ongeldige UTF-8");
-       
+        let text: String = String::from_utf8(hex::decode(hex1).unwrap()).unwrap();
         println!("{}", text);
     }else if args[1] == "--bronkode"{
         println!("bronkode kan gevind word op my github, gebruikersnaam kaszovsky");
